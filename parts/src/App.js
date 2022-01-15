82a5e0490a6d23910439cada316c8d37d09b4cb8
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-01-15 11:13:05 +0300

    Started the exercise

--- parts/src/App.js
+++ parts/src/App.js
@@ -4,23 +4,39 @@ import './App.css';
 
 function App({ notes }) {
 
-  const [note, setnote] = useState(notes)
-   const addNote = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
+  const [note, setNote] = useState(notes)
+  const [newNote, setNewNote] = useState(' ')
+
+  const addNote = (event) => {
+  event.preventDefault()
+  const noteObject = {
+    content: newNote,
+    date: new Date().toISOString(),
+    important: Math.random() < 0.5,
+    id: notes.length + 1,
   }
+
+  setNote(notes.concat(noteObject))
+  setNewNote('')
+}
+
+    const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note => 
-          <Notes key={note.id} note={note} />
+          <Notes key={note.id} note={note}  />
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input />
-        <button type="submit">save</button>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit"  >save</button>
       </form>   
     </div>
 
